14b9e0186586b6cb9686784afe4ec2e7170e4a22
Anna Lyubinska <anna.lyubinskagmail.com>
2025-05-20 20:11:18 +0200

    change temlates location to a separate directory

--- tasks/views.py
+++ tasks/views.py
@@ -12,20 +12,20 @@ from tasks.models import Task, Tag
 
 class TaskListView(generic.ListView):
     model = Task
-    template_name = "index.html"
+    template_name = "tasks/index.html"
     queryset = Task.objects.all().prefetch_related("tags")
 
 
 class TaskCreateView(generic.CreateView):
     model = Task
     fields = "__all__"
-    template_name = "task_form.html"
+    template_name = "tasks/task_form.html"
     success_url = reverse_lazy("index")
 
 
 class TagCreateView(generic.CreateView):
     model = Tag
     fields = "__all__"
-    template_name = "tag_form.html"
+    template_name = "tasks/tag_form.html"
     success_url = reverse_lazy("index")
     pass
